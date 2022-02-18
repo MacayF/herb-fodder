ede35c48ae541dfcc2aac0bc08de6cd544203ec3
Macay Fischer <macayfischer@Macays-MacBook-Pro.local>
2022-02-17 17:01:40 -0800

    search bar intial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react'
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
 
 function App() {
+  const [meal, setMeal] = useState("");
+
+  function search() {
+    console.log(meal);
+  }
+
   return (
     <div className="App">
-
+      <Autocomplete
+        disablePortal
+        id="search-bar"
+        options={['The Godfather', 'Pulp Fiction']}
+        sx={{ width: 300 }}
+        renderInput={(params) => <TextField 
+          {...params} 
+          label="Search"
+          onChangeCapture={(e) => setMeal(e.target.value)}
+          onKeyPress={(e) => {
+            console.log(e);
+            if (e.key === 'Enter') search();
+          }} 
+        />}
+      />
     </div>
   );
 }
