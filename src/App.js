fea30009de7be7d97a31a8bd9e8bc5d69a254b8f
Macay Fischer <macayfischer@Macays-MacBook-Pro.local>
2022-02-25 13:45:59 -0800

    api failed

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
 
 
+
 function App() {
   const [meal, setMeal] = useState("");
+  const [allMeals, setAllMeals] = useState([]);
 
   window.onLoad = loadRecipes();
 
-  function loadRecipes(){
+  async function loadRecipes(){
     console.log('loaded');
+    await fetch("https://edamam-recipe-search.p.rapidapi.com/search?q=vegan", {
+      "method": "GET",
+      "headers": {
+        "x-rapidapi-host": "edamam-recipe-search.p.rapidapi.com",
+        "x-rapidapi-key": "677f84484cmsh1441584038ec2f5p19a6bdjsn0502b775bdc5"
+      }
+    })
+    .then(response => response.json())
+    // .then(data => makeDataSet(data))
+    .then(data => console.log(data))
+    .catch(err => {
+      console.error(err);
+    });
+  }
+
+  function makeDataSet(data) {
+    for(var i = 0; i < 10; i++) {
+      const recipe = data.hits[i];
+      setAllMeals([recipe, ...allMeals]);
+      console.log(allMeals);
+    }
   }
 
   function search() {
@@ -20,11 +43,19 @@ function App() {
 
   return (
     <div className="App">
+      <div className='header'>
+        <img className="logo"/>
+        <p>Herb & Fodder</p>
+      </div>
+      <div className='banner'>
+        <p>Help the Planet.</p>
+        <p>Help Yourself.</p>
+      </div>
       <Autocomplete
         disablePortal
         id="search-bar"
-        options={['The Godfather', 'Pulp Fiction']}
-        sx={{ width: 300 }}
+        options={['Meatloaf', 'Tuna Salad Sandwich', 'ceaser salad']}
+        sx={{ width: 330, bgcolor: 'white' }}
         // sets meal const as input value
         inputValue={meal}
         onInputChange={(event, newInputValue) => {
@@ -39,6 +70,9 @@ function App() {
           }}
         />}
       />
+      <div className='inspo'>
+        <p className='inspo-header'>Some Eco-Inspo</p>
+      </div>
     </div>
   );
 }
